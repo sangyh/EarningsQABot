8d1dd7f1e369faa0f3b038599efd783bbda96982
sangyh89 <sangyh89@gmail.com>
2023-02-17 12:08:41 -0800

    updates to logic and edge case handling in main.py

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ index_new = GPTPineconeIndex.load_from_disk('data/indices/Earnings_index_pinecon
 # From here down is all the StreamLit UI.
 st.set_page_config(page_title="Investing QA Bot", page_icon=":robot:")
 st.header("QA Bot: this chatbot will answer questions based on earnings calls")
-message('Hi I am Mr.Bot. I will share what I know along with sources for all my answers :)')
+message("""Hi I am Mr.Bot. I will share what I know along with sources for all my answers :).
+        PS: I currently only know about AAPL, AMZN and GOOG for 2022 earnings calls.""")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -45,7 +46,7 @@ def get_text():
 def get_metadata(user_query):
     gpt3_response = openai.Completion.create(
                     model="text-davinci-003",
-                    prompt=f"Extract the company symbol, year and quarter from the query below.\nExample: \"What was Amazon's revenue numbers as reported in the third quarter of 2022?\"\nAnswer: {{'symbol': 'AMZN',  'quarter': 3, 'year': 2022}}\n\n{user_query}\nAnswer:",
+                    prompt=f"Extract the company symbol, year and quarter from the query below. If the query is unrelated to a company's earnings, then say \"Please ask about a company's eanrings in a specific year/quarter.\".\nExample: \"What was Amazon's revenue numbers as reported in the third quarter of 2022?\"\nAnswer: {{'symbol': 'AMZN',  'quarter': 3, 'year': 2022}}\n\n{user_query}\nAnswer:",
                     temperature=0,
                     max_tokens=100,
                     top_p=1,
@@ -53,10 +54,13 @@ def get_metadata(user_query):
                     presence_penalty=0
                     )
     string_data = (gpt3_response["choices"][0]["text"].strip())
-    if string_data:
+    print('string_data::', string_data)
+    try:
         return json.loads(string_data.replace("'", "\"")) 
+    except:
+        return string_data
 
-user_input = get_text()
+user_input = st.text_input("You: ") #get_text()
 
 if user_input:
     query = f"""You are a financial agent designed to answer questions about company earnings calls from the provided data.
@@ -65,14 +69,26 @@ Quarters can be referred to as Q1 for first quarter, Q2 for second quarter, Q3 f
 It is very important to first match the user query to the relevant company, year and quarter before proceeding to fetch the answers.
 With this information, answer the following question: {user_input}"""
 
-    query_metadata = get_metadata(user_input)
-    response = index_new.query(query, query_metadata = query_metadata)
+    query_metadata_response = get_metadata(user_input)
     
-    source_info = response.source_nodes[0].source_text.split('\n')
-    doc, quarter, symbol, FY= source_info[0],source_info[1],source_info[2],source_info[4]
-    
-    output = f"Answer: {response.response}\nSources: {doc, quarter, symbol, FY}"
-    print(response)
+    print(type(query_metadata_response))
+
+    if type(query_metadata_response)==dict:
+        query_metadata=query_metadata_response
+
+        response = index_new.query(query, query_metadata = query_metadata)
+        
+        if response.source_nodes:
+            source_info = response.source_nodes[0].source_text.split('\n')
+            doc, quarter, symbol, FY= source_info[0],source_info[1],source_info[2],source_info[4]
+        
+            output = f"Answer: {response.response}\nSources: {doc, quarter, symbol, FY}"
+        else:
+            output = f"Answer: {response.response}\nSources: None found."
+        
+    else:
+        print(query_metadata_response)
+        output = f"Answer: Please retry. {query_metadata_response}"
     st.session_state['past'].append(user_input)
     st.session_state['generated'].append(output)
 
